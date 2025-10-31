b5906a4e60a7522d2e16b19ed57f32f52732d7d6
Zarak <zarak.dev@gmail.com>
2025-10-31 15:18:57 +0500

    Done DataConversion

---
+++ js_course/03_dataconv.js
@@ -0,0 +1,54 @@
+// let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" = 33
+// "33abs" = NaN
+// true = 1
+// false = 0
+
+
+// let IsLoggedIn = undefined
+
+// let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(BooleanIsLoggedIn);
+// console.log(typeof BooleanIsLoggedIn);
+
+
+// 1 = true
+// 0 = false
+// anything in strings  = true
+// empty "" = false
+
+/****************Operations*************/
+
+// let value = 3
+// let NegValue = -value
+
+// console.log(NegValue);
+
+// let str1 = 9
+// let str2 = 8
+// let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log(1 + 7 + "2" + 3 + 4);
+// console.log("4" + 2 + 3);
+// console.log(1 + 5 + "3");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+// console.log(num1);
+
+let gameCounter = 100
+gameCounter ++;
+console.log(gameCounter);
+  
